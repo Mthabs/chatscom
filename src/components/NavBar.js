8611eb4b600d8875b198bf94cb835a4a417de590
Mthabs <>
2023-12-29 20:43:50 +0000

    add navigation bar style

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
