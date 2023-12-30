df3572072438559d71eca1d6d24be0d0752e814f
Mthabs <>
2023-12-30 00:35:09 +0000

    add SignIn form, Handling Events for SignInForm, useContext hook

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://drf-api-rec.herokuapp.com/";
+axios.defaults.baseURL = "https://chatcom-ec4ad238849d.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
