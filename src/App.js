69686c9d2155503c24a99284f8431210ac032ad0
Mthabs <>
2023-12-30 03:00:24 +0000

    add videos upload form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import styles from "../../App.module.css";
+import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
@@ -7,6 +7,7 @@ import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
 import PhotoUploadForm from "./pages/photos/PhotoUploadForm";
+import VideoUploadForm from "./pages/videos/VideoUploadForm";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
           <Route exact path="/photos/upload" render={() => <PhotoUploadForm />} />
+          <Route exact path="/videos/upload" render={() => <VideoUploadForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
