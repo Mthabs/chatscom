12910454945200d0e90b49bf4ebf6ab5abc7b3d4
Mthabs <>
2023-12-30 02:45:38 +0000

    add photos upload form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import styles from "./App.module.css";
+import styles from "../../App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
@@ -6,6 +6,7 @@ import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
+import PhotoUploadForm from "./pages/photos/PhotoUploadForm";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+          <Route exact path="/photos/upload" render={() => <PhotoUploadForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
