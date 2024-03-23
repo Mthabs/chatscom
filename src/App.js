f3bda4d9c3c339ca14a7ed7b7787b7e8c23a04bd
Thabani Nhleko <mthabs1@gmail.com>
2024-03-23 14:05:08 +0530

    refactor: clean routes in app.js

--- src/App.js
+++ src/App.js
@@ -6,90 +6,16 @@ import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/register/Register";
 import SignInForm from "./pages/auth/login/login";
 
-import PostCreateForm from "./pages/posts/PostCreateForm";
-import PostPage from "./pages/posts/PostPage";
-import PostsPage from "./pages/posts/PostsPage";
-import PhotoUploadForm from "./pages/photos/PhotoUploadForm";
-import PhotoPage from "./pages/photos/PhotoPage";
-import PhotosPage from "./pages/photos/PhotosPage"; 
-import VideosPage from "./pages/videos/VideosPage";
-import VideoUploadForm from "./pages/videos/VideoUploadForm";
-import VideoPage from "./pages/videos/VideoPage";
-import { useCurrentUser } from "./contexts/CurrentUserContext";
-import PostEditForm from "./pages/posts/PostEditForm";
-import PhotoEditForm from "./pages/photos/PhotoEditForm";
-import VideoEditForm from "./pages/videos/VideoEditForm";
-import ProfilePage from "./pages/profiles/ProfilePage";
-import UsernameForm from "./pages/profiles/UsernameForm";
-import UserPasswordForm from "./pages/profiles/UserPasswordForm";
-import ProfileEditForm from "./pages/profiles/ProfileEditForm";
-import Friends from './pages/friends/Friends';
+
 
 function App() {
-  const currentUser = useCurrentUser();
-  const profile_id = currentUser?.profile_id || "";
-
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route
-            exact
-            path="/"
-            render={() => (
-              <PostsPage message="No results found. Adjust the search keyword." />
-            )}
-          />
-          <Route
-            exact
-            path="/feed"
-            render={() => (
-              <PostsPage
-                message="No results found. Adjust the search keyword or follow a user."
-                filter={`owner__followed__owner__profile=${profile_id}&`}
-              />
-            )}
-          />
-          <Route
-            exact
-            path="/liked"
-            render={() => (
-              <PostsPage
-                message="No results found. Adjust the search keyword or like a post."
-                filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
-              />
-            )}
-          />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
-          <Route exact path="/posts/create" render={() => <PostCreateForm />} />
-          <Route exact path="/posts/:id" render={() => <PostPage />} />
-          <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
-          <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
-          <Route exact path="/photos/upload" render={() => <PhotoUploadForm />} />
-          <Route exact path="/photos/:id" render={() => <PhotoPage />} />
-          <Route exact path="/photos/:id/edit" render={() => <PhotoEditForm />} />
-          <Route exact path="/videos/upload" render={() => <VideoUploadForm />} />
-          <Route exact path="/videos/:id" render={() => <VideoPage />} />
-          <Route exact path="/videos/:id/edit" render={() => <VideoEditForm />} />
-          <Route path="/friends" render={() => <Friends />} />
-          <Route
-            exact
-            path="/profiles/:id/edit/username"
-            render={() => <UsernameForm />}
-          />
-          <Route
-            exact
-            path="/profiles/:id/edit/password"
-            render={() => <UserPasswordForm />}
-          />
-          <Route
-            exact
-            path="/profiles/:id/edit"
-            render={() => <ProfileEditForm />}
-          />
-
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
