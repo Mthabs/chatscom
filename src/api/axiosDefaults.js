678503421220ec8deef0688dc9e6839532af908a
Thabani Nhleko <mthabs1@gmail.com>
2024-03-23 13:32:07 +0530

    refactor: modified axios

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,8 +1,29 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://chatcomdrfapi-09a2723b7b7c.herokuapp.com/";
+// axios.defaults.baseURL = "https://chatcom-backend-8579ef0ee515.herokuapp.com/";
+axios.defaults.baseURL = "http://localhost:8000/";
+axios.defaults.mode = "no-cors"
+axios.defaults.withCredentials = false
+axios.defaults.credentials ="include" 
+
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+
+export const customaxios = axios.create();
+customaxios.interceptors.request.use(
+    (config) => {
+      // Retrieve the auth token from session storage
+      const authToken = sessionStorage.getItem("authtoken");
+      // If the auth token exists, add it to the request headers
+      if (authToken) {
+        config.headers.Authorization = `Token ${authToken}`;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+  });
 
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
