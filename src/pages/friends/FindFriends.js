74ed0eb17a23350beee2aa6a0b89aea67535b5fc
Thabani Nhleko <mthabs1@gmail.com>
2024-03-24 17:59:37 +0530

    fix: fixed find friends

--- src/pages/friends/FindFriends.js
+++ src/pages/friends/FindFriends.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { customaxios } from '../../api/axiosDefaults';
-import UserList from './userList';
+import ListGroup from 'react-bootstrap/ListGroup';
+import styles from "../../styles/Avatar.module.css";
 
 
 const FindFriends = () => {
@@ -36,7 +37,24 @@ const FindFriends = () => {
         onChange={(e) => setSearchQuery(e.target.value)}
         className='mb-2'
       />
-       <UserList data={data} underFlowMessage={"No Match to that Query"} />
+      <ListGroup variant="flush">
+        { data.map((user)=>{
+          const {id, profile_picture, full_name} = user
+          return(
+            <ListGroup.Item>
+              <div className='d-flex justify-content-between'>
+                <div className='d-flex'>
+                  {!profile_picture && <img src='https://res.cloudinary.com/dnt7oro5y/image/upload/v1/default_profile_qdjgyp'  className={styles.Avatar}  alt="Profile Picture" style={{ width: '50px', height: '50px', marginRight: '10px' }}  /> }
+                  {profile_picture && <img src={profile_picture} alt="Profile Picture" className={styles.Avatar} style={{ width: '50px', height: '50px', marginRight: '10px' }} /> }
+                  <a href={'/profile/' + id} ><span className='mt-2'>{full_name}</span></a>
+                </div>
+              </div>
+            </ListGroup.Item>
+          )})}
+     </ListGroup>
+
+     { data.length === 0 && <span> No Match to that Query </span> }
+       
     </div>
   );
 };
